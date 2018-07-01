3779b36a2a0dbcd1ebdc68366f0b386866ce1190
lxx.air <lxx2013@mail.ustc.edu.cn>
2018-07-01 15:45:41 +0800

    主页初始化，样式待调，爬虫没写

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img id="avatar" src="https://avatars2.githubusercontent.com/u/20529542">
+  <el-row id="collapse">
+<el-col :span="24"><Collapse></Collapse></el-col>
+</el-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Collapse from "./components/collapse"
 export default {
   name: 'app',
   components: {
-    HelloWorld
+     Collapse
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +25,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#collapse{
+  max-width:414px;
+  margin:0 auto;
+}
+#avatar{
+  width:50%;
+  max-width:200px;
 }
 </style>
