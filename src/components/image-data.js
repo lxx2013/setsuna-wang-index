a71e2cce2814f8dc660c324087427deabf19db49
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-07-30 19:17:29 +0800

    update bilibili

--- src/components/image-data.js
+++ src/components/image-data.js
@@ -3,8 +3,8 @@ export default [
 		"src": "星奏歌封面.jpeg",
 		"href":'http://www.bilibili.com/video/av26217050',
 		"title":'星の奏でる歌-吉他(附谱)',
-		"number":51,
-		"time":'18小时前',
+		"number":192,
+		"time":'7-5',
 	},
 	
 ]
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     baseUrl: './',
-    outputDir:"docs/"
+    outputDir:"docs/",
+    lintOnSave: false
 }
